83ae0df0a9f25d79aa4b3eb9d4be4a9d69802105
Alex <y10b@ya.ru>
2024-05-17 17:48:12 +0200

    initial

---
+++ CustomProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomProject
+//
+//  Created by Alex on 17.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
